efdbf94554f0a5559665a2a5fcba8f17a94763bd
Yurucamp123 <lethanhbinh6122003@gmail.com>
2025-08-10 13:17:07 +0700

    Fix footer

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -3,6 +3,7 @@ import Button from "../button/Button";
 import styles from "./Footer.module.css";
 import InputSendEmail from "./InputSendEmail";
 import Link from "next/link";
+import TikTokIcon from "@/public/assets/icon/TiktokIcon";
 
 const contactInfo = [
   {
@@ -52,7 +53,10 @@ export default function Footer() {
               </h6>
               <ul className="menu-footer">
                 <li className="mb-10">
-                  <a target="_blank" href="https://firebasestorage.googleapis.com/v0/b/love-fe-71303.firebasestorage.app/o/footer%2FCh%C3%ADnh%20s%C3%A1ch%20b%E1%BA%A3o%20m%E1%BA%ADt.pdf?alt=media&token=ad61dcd7-1942-4aa1-91c1-edeefbea3d86">
+                  <a
+                    target="_blank"
+                    href="https://firebasestorage.googleapis.com/v0/b/love-fe-71303.firebasestorage.app/o/footer%2FCh%C3%ADnh%20s%C3%A1ch%20b%E1%BA%A3o%20m%E1%BA%ADt.pdf?alt=media&token=ad61dcd7-1942-4aa1-91c1-edeefbea3d86"
+                  >
                     Chính sách bảo mật thông tin
                   </a>
                 </li>
@@ -65,7 +69,10 @@ export default function Footer() {
               </h6>
               <ul className="menu-footer">
                 <li className="mb-10">
-                  <a target="_blank" href="https://firebasestorage.googleapis.com/v0/b/love-fe-71303.firebasestorage.app/o/footer%2FCh%C3%ADnh%20s%C3%A1ch%20nh%C3%A0%20t%E1%BB%95%20ch%E1%BB%A9c.pdf?alt=media&token=df78195b-75c5-42af-83bd-29b4e303f616">
+                  <a
+                    target="_blank"
+                    href="https://firebasestorage.googleapis.com/v0/b/love-fe-71303.firebasestorage.app/o/footer%2FCh%C3%ADnh%20s%C3%A1ch%20nh%C3%A0%20t%E1%BB%95%20ch%E1%BB%A9c.pdf?alt=media&token=df78195b-75c5-42af-83bd-29b4e303f616"
+                  >
                     Điều khoản sử dụng cho ban tổ chức
                   </a>
                 </li>
@@ -78,7 +85,10 @@ export default function Footer() {
               </h6>
               <ul className="menu-footer">
                 <li className="mb-10">
-                  <a target="_blank" href="https://firebasestorage.googleapis.com/v0/b/love-fe-71303.firebasestorage.app/o/footer%2FCh%C3%ADnh%20s%C3%A1ch%20kh%C3%A1ch%20h%C3%A0ng.pdf?alt=media&token=7e3977ef-9572-4ea6-b1b9-4ee7e1a1f1b0">
+                  <a
+                    target="_blank"
+                    href="https://firebasestorage.googleapis.com/v0/b/love-fe-71303.firebasestorage.app/o/footer%2FCh%C3%ADnh%20s%C3%A1ch%20kh%C3%A1ch%20h%C3%A0ng.pdf?alt=media&token=7e3977ef-9572-4ea6-b1b9-4ee7e1a1f1b0"
+                  >
                     Điều khoản sử dụng cho Khách hàng
                   </a>
                 </li>
@@ -94,12 +104,18 @@ export default function Footer() {
               </h6>
               <ul className="menu-footer">
                 <li className="mb-10">
-                  <a target="_blank" href="https://firebasestorage.googleapis.com/v0/b/love-fe-71303.firebasestorage.app/o/footer%2FH%E1%BB%97%20tr%E1%BB%A3%20%C4%91%E1%BA%B7t%20v%C3%A9.pdf?alt=media&token=60ab3fbe-799a-4810-938e-f3fe3b17cdd3">
+                  <a
+                    target="_blank"
+                    href="https://firebasestorage.googleapis.com/v0/b/love-fe-71303.firebasestorage.app/o/footer%2FH%E1%BB%97%20tr%E1%BB%A3%20%C4%91%E1%BA%B7t%20v%C3%A9.pdf?alt=media&token=60ab3fbe-799a-4810-938e-f3fe3b17cdd3"
+                  >
                     Hướng dẫn đặt vé
                   </a>
                 </li>
                 <li>
-                  <a target="_blank" href="https://firebasestorage.googleapis.com/v0/b/love-fe-71303.firebasestorage.app/o/footer%2FH%E1%BB%97%20tr%E1%BB%A3%20t%E1%BA%A1o%20ws.pdf?alt=media&token=982fda96-9ced-4da7-b769-efe719e5b48f">
+                  <a
+                    target="_blank"
+                    href="https://firebasestorage.googleapis.com/v0/b/love-fe-71303.firebasestorage.app/o/footer%2FH%E1%BB%97%20tr%E1%BB%A3%20t%E1%BA%A1o%20ws.pdf?alt=media&token=982fda96-9ced-4da7-b769-efe719e5b48f"
+                  >
                     Hướng dẫn tạo workshop
                   </a>
                 </li>
@@ -127,21 +143,11 @@ export default function Footer() {
                   Follow us
                 </p>
                 <div className="box-socials-footer d-inline-block">
-                  <Link className="icon-socials icon-instagram" href="#">
-                    <svg
-                      width={20}
-                      height={20}
-                      viewBox="0 0 20 20"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M13.4915 1.6665H6.50817C3.47484 1.6665 1.6665 3.47484 1.6665 6.50817V13.4832C1.6665 16.5248 3.47484 18.3332 6.50817 18.3332H13.4832C16.5165 18.3332 18.3248 16.5248 18.3248 13.4915V6.50817C18.3332 3.47484 16.5248 1.6665 13.4915 1.6665ZM9.99984 13.2332C8.2165 13.2332 6.7665 11.7832 6.7665 9.99984C6.7665 8.2165 8.2165 6.7665 9.99984 6.7665C11.7832 6.7665 13.2332 8.2165 13.2332 9.99984C13.2332 11.7832 11.7832 13.2332 9.99984 13.2332ZM14.9332 5.73317C14.8915 5.83317 14.8332 5.92484 14.7582 6.00817C14.6748 6.08317 14.5832 6.1415 14.4832 6.18317C14.3832 6.22484 14.2748 6.24984 14.1665 6.24984C13.9415 6.24984 13.7332 6.1665 13.5748 6.00817C13.4998 5.92484 13.4415 5.83317 13.3998 5.73317C13.3582 5.63317 13.3332 5.52484 13.3332 5.4165C13.3332 5.30817 13.3582 5.19984 13.3998 5.09984C13.4415 4.9915 13.4998 4.90817 13.5748 4.82484C13.7665 4.63317 14.0582 4.5415 14.3248 4.59984C14.3832 4.60817 14.4332 4.62484 14.4832 4.64984C14.5332 4.6665 14.5832 4.6915 14.6332 4.72484C14.6748 4.74984 14.7165 4.7915 14.7582 4.82484C14.8332 4.90817 14.8915 4.9915 14.9332 5.09984C14.9748 5.19984 14.9998 5.30817 14.9998 5.4165C14.9998 5.52484 14.9748 5.63317 14.9332 5.73317Z"
-                        fill
-                      />
-                    </svg>
-                  </Link>
-                  <Link className="icon-socials icon-facebook" href="#">
+                  <a
+                    target="_blank"
+                    className="icon-socials icon-facebook"
+                    href="https://www.facebook.com/profile.php?id=61576189272965"
+                  >
                     <svg
                       width={20}
                       height={20}
@@ -154,35 +160,14 @@ export default function Footer() {
                         fill
                       />
                     </svg>
-                  </Link>
-                  <Link className="icon-socials icon-twitter" href="#">
-                    <svg
-                      width={18}
-                      height={16}
-                      viewBox="0 0 18 16"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M17.1924 3.06705L14.8565 5.40299C14.3846 10.8733 9.77132 15.1249 4.25022 15.1249C3.11585 15.1249 2.18069 14.9452 1.47053 14.5905C0.897878 14.3038 0.663503 13.9967 0.604909 13.9092C0.552663 13.8309 0.518798 13.7417 0.505847 13.6485C0.492895 13.5552 0.50119 13.4602 0.530113 13.3706C0.559036 13.2809 0.607839 13.199 0.672875 13.1309C0.737911 13.0628 0.817498 13.0102 0.905691 12.9772C0.926003 12.9694 2.79944 12.2499 3.98928 10.8803C3.32943 10.3378 2.75341 9.70072 2.27991 8.98971C1.31116 7.55143 0.226784 5.05299 0.561159 1.3194C0.571758 1.20076 0.616036 1.08762 0.688779 0.993308C0.761521 0.898992 0.859699 0.827427 0.971752 0.787039C1.0838 0.746651 1.20506 0.739122 1.32125 0.76534C1.43744 0.791557 1.54372 0.850429 1.62757 0.935022C1.65491 0.962365 4.22757 3.52096 7.37288 4.35065V3.87487C7.37168 3.37595 7.47032 2.88185 7.66299 2.42164C7.85566 1.96143 8.13847 1.54443 8.49475 1.19518C8.84077 0.849652 9.25248 0.576929 9.70561 0.393103C10.1587 0.209277 10.6441 0.11807 11.133 0.124865C11.7889 0.131335 12.432 0.307407 12.9997 0.635963C13.5674 0.964519 14.0405 1.43438 14.3729 1.99987H16.7502C16.8739 1.99977 16.9948 2.03637 17.0977 2.10504C17.2006 2.17371 17.2808 2.27136 17.3281 2.38562C17.3755 2.49989 17.3878 2.62563 17.3637 2.74693C17.3395 2.86823 17.2799 2.97964 17.1924 3.06705Z"
-                        fill
-                      />
-                    </svg>
-                  </Link>
-                  <Link className="icon-socials icon-be" href="#">
-                    <svg
-                      width={21}
-                      height={15}
-                      viewBox="0 0 21 15"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M8.82393 10.736L13.9225 7.78881L8.82393 4.84165V10.736ZM20.1803 3.04389C20.308 3.50561 20.3964 4.12451 20.4554 4.91042C20.5242 5.69633 20.5536 6.37418 20.5536 6.96361L20.6126 7.78881C20.6126 9.94024 20.4554 11.5219 20.1803 12.5337C19.9347 13.4179 19.3649 13.9877 18.4808 14.2333C18.0191 14.361 17.1742 14.4494 15.8775 14.5083C14.6004 14.5771 13.4313 14.6066 12.3507 14.6066L10.7887 14.6655C6.67251 14.6655 4.10848 14.5083 3.09662 14.2333C2.21247 13.9877 1.64269 13.4179 1.39709 12.5337C1.26938 12.072 1.18097 11.4531 1.12203 10.6672C1.05326 9.8813 1.02379 9.20345 1.02379 8.61402L0.964844 7.78881C0.964844 5.63739 1.12203 4.05575 1.39709 3.04389C1.64269 2.15974 2.21247 1.58996 3.09662 1.34436C3.55834 1.21665 4.4032 1.12823 5.69995 1.06929C6.97705 1.00052 8.14609 0.971052 9.22671 0.971052L10.7887 0.912109C14.9049 0.912109 17.4689 1.06929 18.4808 1.34436C19.3649 1.58996 19.9347 2.15974 20.1803 3.04389Z"
-                        fill
-                      />
-                    </svg>
-                  </Link>
+                  </a>
+                  <a
+                    target="_blank"
+                    className="icon-socials icon-be"
+                    href="https://www.tiktok.com/@lovebookingws"
+                  >
+                    <TikTokIcon />
+                  </a>
                 </div>
               </div>
             </div>
